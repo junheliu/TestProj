9ae6312fa9bef6c7f937e60551211bacfc91e569
junhe <junhe@junhe-Lenovo-G40-30>
2018-01-13 11:01:42 -0800

    This is a second commit

---
+++ hi/src/hi/Main.java
@@ -0,0 +1,14 @@
+package hi;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello there!");
+		System.out.println("this is a test");
+		System.out.println("this is another test");
+
+	}
+
+}
